2361c78b93ab0b5f85cf0337859dc82181657c66
vvr3ddy <vvr-rockstar@bmsit.in>
2021-05-14 10:28:24 +0530

    :octocat: Services: Inject the services
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { FreelancerComponent } from './component/freelancer/freelancer.component';
-import { FreelancerModule } from './module/freelancer/freelancer.module';
 import { HomeComponent } from './components/home/home.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
+
 import { AddBookmarkfreelancerComponent } from './modules/add-bookmarkfreelancer/add-bookmarkfreelancer.component';
 import { DeleteBookmarkfreelancerComponent } from './modules/delete-bookmarkfreelancer/delete-bookmarkfreelancer.component';
 import { FindBookmarkfreelancerByskillComponent } from './modules/find-bookmarkfreelancer-byskill/find-bookmarkfreelancer-byskill.component';
 import { FindBookmarkfreelancerByidComponent } from './modules/find-bookmarkfreelancer-byid/find-bookmarkfreelancer-byid.component';
+import { AdminService } from './services/admin.service'
+import { BookmarkedfreelancerService } from './services/bookmarkedfreelancer.service'
+import { BookmarkedjobService } from './services/bookmarkedjob.service'
+import { FeedbackService } from './services/feedback.service'
+import { FreelancerService } from './services/freelancer.service'
+import { JobService } from './services/job.service'
+import { JobapplicationService } from './services/jobapplication.service'
+import { RecruiterService } from './services/recruiter.service'
+import { SkillService } from './services/skill.service'
+import { SkillexperienceService } from './services/skillexperience.service'
 
 @NgModule({
   declarations: [
     AppComponent,
-    FreelancerComponent,
     HomeComponent,
     AboutUsComponent,
     ContactUsComponent,
@@ -28,9 +37,19 @@ import { FindBookmarkfreelancerByidComponent } from './modules/find-bookmarkfree
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FreelancerModule
   ],
-  providers: [],
+  providers: [
+    AdminService,
+    BookmarkedfreelancerService,
+    BookmarkedjobService,
+    FeedbackService,
+    FreelancerService,
+    JobService,
+    JobapplicationService,
+    RecruiterService,
+    SkillService,
+    SkillexperienceService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
