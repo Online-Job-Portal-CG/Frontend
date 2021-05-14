a2c50d7c597917ebb1a890f5ee90d8c54604f2e6
vvr3ddy <vvr-rockstar@bmsit.in>
2021-05-14 15:39:24 +0530

    :octocat: Services: Create BookmarkedFreelancer Services
    + Add CRUD Methods for BookmarkedFreelancer Module
    
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

--- src/app/services/bookmarkedfreelancer.service.ts
+++ src/app/services/bookmarkedfreelancer.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class BookmarkedfreelancerService {
+  private baseUrl = "http://localhost:8001/bmark/freelancer";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // POST METHOD
+  public addBookmarkFreelancer(bMark: BookmarkFreelancer): Observable<any> {
+    console.log("Add bookmark Freelancer method");
+    const headers = new HttpHeaders().set('Content_Type ', 'application/json; charset=utf-8');
+    return this.http.post(`${this.baseUrl}/add`, bMark, { headers, responseType: 'json' });
+  }
+
+  //DELETE METHOD
+  public deleteBookmarkFreelancer(id: number): Observable<any> {
+    console.log("Delete bookmark Freelancer method");
+    const headers = new HttpHeaders().set('Content_Type ', 'application/json; charset=utf-8');
+    return this.http.delete(`${this.baseUrl}/delete/${id}`, { headers, responseType: 'json' });
+  }
+
+  //GET METHOD
+  public getBookmarkedFreelancersBySkill(skillName: string): Observable<any> {
+    console.log("Bookmarked Freelancers by skill Name method");
+    const headers = new HttpHeaders().set('Content_Type ', 'application/json; charset=utf-8');
+    return this.http.get(`${this.baseUrl}/findBySkill/${skillName}`, { headers, responseType: 'json' });
+  }
+
+  //GET METHOD
+  public getBookmarkedFreelancerById(id: number): Observable<any> {
+    console.log("Get Bookmarked Freelancer By Id");
+    const headers = new HttpHeaders().set('Content_Type ', 'application/json; charset=utf-8');
+    return this.http.get(`${this.baseUrl}/get/${id}`, { headers, responseType: 'json' });
+  }
 }
+
+
+export class BookmarkFreelancer {
+  skillId: number;
+  recruiterId: number;
+  freelancerId: number;
+}
+
