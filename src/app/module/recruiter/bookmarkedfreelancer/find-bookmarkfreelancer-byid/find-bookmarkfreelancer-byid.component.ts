3aaa773ad79a9c2b559b4999303fb1042312083c
vvr3ddy <vvr-rockstar@bmsit.in>
2021-05-16 15:46:00 +0530

    :octocat: BookmarkedFreelancer: Create Service Implementations for Frontend
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

--- src/app/module/recruiter/bookmarkedfreelancer/find-bookmarkfreelancer-byid/find-bookmarkfreelancer-byid.component.ts
+++ src/app/module/recruiter/bookmarkedfreelancer/find-bookmarkfreelancer-byid/find-bookmarkfreelancer-byid.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BookmarkedfreelancerService, BookmarkFreelancer } from 'src/app/services/bookmarkedfreelancer.service';
 
 @Component({
   selector: 'app-find-bookmarkfreelancer-byid',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./find-bookmarkfreelancer-byid.component.css']
 })
 export class FindBookmarkfreelancerByidComponent implements OnInit {
+  id: number;
+  bookmarkedFreelancer: BookmarkFreelancer = new BookmarkFreelancer();
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router, private bookmarkedFreelancerService: BookmarkedfreelancerService) { }
 
   ngOnInit(): void {
+    this.bookmarkedFreelancer = new BookmarkFreelancer();
+    this.id = this.route.snapshot.params['id'];
+    this.bookmarkedFreelancerService.getBookmarkedFreelancerById(this.id)
+      .subscribe(data => {
+        console.log(data);
+        this.bookmarkedFreelancer = data;
+      }, err => console.log(err));
   }
-
+  
 }
--- src/app/module/recruiter/bookmarkedfreelancer/find-bookmarkfreelancer-byskill/find-bookmarkfreelancer-byskill.component.ts
+++ src/app/module/recruiter/bookmarkedfreelancer/find-bookmarkfreelancer-byskill/find-bookmarkfreelancer-byskill.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { BookmarkedfreelancerService, BookmarkFreelancer } from 'src/app/services/bookmarkedfreelancer.service';
 
 @Component({
   selector: 'app-find-bookmarkfreelancer-byskill',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./find-bookmarkfreelancer-byskill.component.css']
 })
 export class FindBookmarkfreelancerByskillComponent implements OnInit {
-
-  constructor() { }
+  id: number
+  skillName: string
+  bookmarkedFreelancers: Observable<BookmarkFreelancer[]>;
+  constructor(private route: ActivatedRoute, private router: Router, private bookmarkedFreelancerService: BookmarkedfreelancerService) { }
 
   ngOnInit(): void {
+    //TODO
+    this.bookmarkedFreelancers = new Observable<BookmarkFreelancer[]>();
+    this.skillName = this.route.snapshot.params['skillName'];
+    this.bookmarkedFreelancerService.getBookmarkedFreelancersBySkill(this.skillName)
+      .subscribe(data => {
+        console.log(data);
+        this.bookmarkedFreelancers = data;
+      }, err => console.log(err));
+  }
+
+  reloadData() {
+    this.skillName = this.route.snapshot.params['skillName'];
+    this.bookmarkedFreelancers = this.bookmarkedFreelancerService.getBookmarkedFreelancersBySkill(this.skillName);
+  }
+
+  deleteFreelancer(id: number) {
+    this.bookmarkedFreelancerService.deleteBookmarkFreelancer(id)
+      .subscribe(data => {
+        console.log(data);
+        this.reloadData();
+      }, err => console.log(err));
+  }
+
+  freelancerDetails(id: number) {
+    this.router.navigate(['details', id]);
   }
 
 }
+
