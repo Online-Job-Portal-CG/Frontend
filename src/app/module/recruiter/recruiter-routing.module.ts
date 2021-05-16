8125741b5774194a614df75fb1b959094dbb2a16
vvr3ddy <vvr-rockstar@bmsit.in>
2021-05-16 07:47:21 +0530

    :octocat: Create routes for recruiter controlled components
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

--- src/app/module/recruiter/recruiter-routing.module.ts
+++ src/app/module/recruiter/recruiter-routing.module.ts
@@ -2,29 +2,57 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddBookmarkfreelancerComponent } from './bookmarkedfreelancer/add-bookmarkfreelancer/add-bookmarkfreelancer.component';
 import { BookmarkedfreelancerComponent } from './bookmarkedfreelancer/bookmarkedfreelancer.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 import { FindBookmarkfreelancerByidComponent } from './bookmarkedfreelancer/find-bookmarkfreelancer-byid/find-bookmarkfreelancer-byid.component';
 import { FindBookmarkfreelancerByskillComponent } from './bookmarkedfreelancer/find-bookmarkfreelancer-byskill/find-bookmarkfreelancer-byskill.component';
-
+import { JobComponent } from './job/job.component'
 const routes: Routes = [
-  {path:'', redirectTo:'recruiter', pathMatch:'full'},
+  //base path
+  { path: '', redirectTo: 'recruiter', pathMatch: 'full' },
+  //path to bookmarkfreelancer and its services
   {
-    path:'/bmark/freelancer',
+    path: '/bmark/freelancer',
     component: BookmarkedfreelancerComponent,
-    children:[
+    children: [
       {
-        path:'/add',
-        component:AddBookmarkfreelancerComponent
+        path: '/add',
+        component: AddBookmarkfreelancerComponent
       },
       {
-        path:'/find/:id',
-        component:FindBookmarkfreelancerByidComponent
+        path: '/find/:id',
+        component: FindBookmarkfreelancerByidComponent
       },
       {
-        path:'/find/:skillName',
-        component:FindBookmarkfreelancerByskillComponent
+        path: '/find/:skillName',
+        component: FindBookmarkfreelancerByskillComponent
+      }
+    ],
+  },
+  //path to feedback and its services
+  {
+    path: '/feedback',
+    component: FeedbackComponent,
+    children: [
+      {
+
+      },
+      {
+
       }
     ]
+  },
+  //path to job and its services
+  {
+    path: '/job',
+    component: JobComponent,
+    children: [
+      {
 
+      },
+      {
+
+      }
+    ]
   }
 ];
 
